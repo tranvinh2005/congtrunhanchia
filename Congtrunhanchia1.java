5d217969be7ebc0c87cb5308c915ab33dc58e916
Trần Vinh <151425441+tranvinh2005@users.noreply.github.com>
2024-08-21 19:53:53 +0700

    Add files via upload

---
+++ Congtrunhanchia1.java
@@ -0,0 +1,34 @@
+
+package congtrunhanchia1;
+
+
+       
+
+        import java.awt.BorderLayout;
+        import java.util.Scanner;
+
+        import java.util.Scanner;
+
+        public class Congtrunhanchia1 {
+
+   
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+            System.out.println("nhap vao so nguyen a la ");
+                  int a = keyboard.nextInt();
+            System.out.println("nhap vao so nguyen b");
+                  int b= keyboard.nextInt();
+                  int c= a+b;
+            System.out.println("a+b ="+c);
+                  int d= a-b;
+            System.out.println("a-b ="+d);
+                    int e= a*b;
+            System.out.println("a*b ="+e);
+                    int f= a/b;
+            System.out.println("a/b ="+f);
+                    int g= a%b;
+            System.out.println("a%b ="+g);
+    }
+    
+}
+
